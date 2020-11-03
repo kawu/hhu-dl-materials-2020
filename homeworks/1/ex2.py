437a4af3a53a541eae309fbe1194c09a4b913d91
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2020-11-03 10:59:18 +0100

    Add second exercise

---
+++ homeworks/1/ex2.py
@@ -0,0 +1,34 @@
+import torch
+
+
+def ata(M: torch.Tensor, w: torch.Tensor) -> torch.Tensor:
+    """Apply matrix `M` to each word vector in matrix `w`.
+
+    Examples:
+    >>> w = torch.tensor([[1, 2, 3], [2, 3, 1]], dtype=torch.float)
+    >>> M = torch.zeros(3, 3)
+    >>> ata(M, w)
+    tensor([[0., 0., 0.],
+            [0., 0., 0.]])
+
+    >>> M = torch.ones(3, 3)
+    >>> torch.mv(M, w[0])
+    tensor([6., 6., 6.])
+    >>> torch.mv(M, w[1])
+    tensor([6., 6., 6.])
+    >>> ata(M, w)
+    tensor([[6., 6., 6.],
+            [6., 6., 6.]])
+
+    >>> M = torch.eye(3, dtype=torch.float)
+    >>> assert (ata(M, w) == w).all()
+
+    >>> M = torch.randn(3, 3)
+    >>> for i in range(2):
+    ...     assert (torch.mv(M, w[i]) == ata(M, w)[i]).all()
+    """
+    # Make sure the shapes match
+    assert M.dim() == w.dim() == 2
+    assert M.shape[1] == w.shape[1]
+    # TODO:
+    pass

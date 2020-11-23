a8dcbc9b10958ad0c011b96aaee2b605d3644282
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2020-11-23 13:33:14 +0100

    U4, exercise 1

---
+++ homeworks/4/ex1.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+
+
+def mk_model(alpha_size: int, emb_size: int, class_num: int):
+    """Construct a neural language prediction model.
+
+    Arguments:
+    * alpha_size: alphtabet size (number of different characters)
+    * emb_size: size of vector representations assigned to the different characters
+    * class_num: number of output classes (languages)
+
+    Output: A neural model which transforms a tensor vector with
+    input character indices, such as `tensor([0, 1, 2, 1])`, to
+    a score vector, such as `tensor([0.6861, -0.7673,  1.2969])`. 
+    
+    Examples:
+
+    # Create a language prediction model for 10 distinct characters
+    # and 5 distinct languages
+    >>> alpha_size = 10
+    >>> emb_size = 5
+    >>> class_num = 5
+    >>> model = mk_model(alpha_size, emb_size, class_num)
+
+    # The model should be a nn.Module
+    >>> isinstance(model, nn.Module)
+    True
+
+    # The size of the output score vector should be equal to `class_num`.
+    >>> len(model(torch.tensor([0, 1, 2]))) == class_num
+    True
+    """
+    pass

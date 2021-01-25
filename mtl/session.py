d32e8ba4ab545e68aaf89724f85b1c320648081d
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2021-01-25 16:52:56 +0100

    Upload code from the MTL stream (part 2)

--- mtl/session.py
+++ mtl/session.py
@@ -21,29 +21,98 @@ enc_dev = encode_with(dev_data, char_enc, pos_enc)
 print("# Train size =", len(enc_train))
 print("# Dev size =", len(enc_dev))
 
-model = nn.Sequential(
-    Map(nn.Sequential(
-        nn.Embedding(char_enc.size()+1, 50, padding_idx=char_enc.size()),
-        SimpleLSTM(50, 200),
-        Apply(lambda xs: xs[-1]),
-    )),
-    SimpleBiLSTM(200, 200),
-    Biaffine(200),
-)
+class Joint(nn.Module):
+    """Joint POS tagging / dependency parsing module.
+
+    Type: EncInp -> Tuple[PosScores, DepScores]
+
+    where:
+
+    * EncInp is an encoded input sentence (list of tensors)
+    * PosScores is a Tensor[N x T] with POS-related scores,
+      where T is the number of distinct POS tags.
+    * DepScores is a Tensor[N x (N + 1)] of dependency-related scores,
+      one score vector of size (N + 1) per word.
+
+    The two components (POS tagging and dependency parsing) are based
+    on a common contextualized embedding representation.
+    """
+
+    def __init__(self,
+        char_enc: Encoder[Char],    # Encoder for input characters
+        pos_enc: Encoder[POS],      # Encoder for POS tags
+        emb_size: int,              # Embedding size
+        hid_size: int               # Hidden size used in LSTMs
+    ):
+        super().__init__()
+
+        # Keep encoding objects for future use
+        self.char_enc = char_enc
+        self.pos_enc = pos_enc
+
+        # Common part of the model: embedding and LSTM contextualization
+        self.embed = nn.Sequential(
+            Map(nn.Sequential(
+                nn.Embedding(char_enc.size()+1, emb_size, padding_idx=char_enc.size()),
+                SimpleLSTM(emb_size, hid_size),
+                Apply(lambda xs: xs[-1]),
+            )),
+            SimpleBiLSTM(hid_size, hid_size),
+        )
+
+        # Scoring module for the POS tagging task
+        self.score_pos = nn.Linear(hid_size, pos_enc.size())
+
+        # Biaffine dependency scoring module
+        self.score_dep = Biaffine(hid_size)
+
+    def forward(self, xs: EncInp) -> Tuple[Tensor, Tensor]:
+        embs = self.embed(xs)
+        return (self.score_pos(embs), self.score_dep(embs))
+
+    @torch.no_grad()
+    def tag(self, sent: List[Word]) -> List[POS]:
+        """Tag a sentence with POS tags."""
+        xs = encode_input(sent, self.char_enc)
+        embs = self.embed(xs)
+        scores = self.score_pos(embs)
+        ys = torch.argmax(scores, dim=1)
+        return [self.pos_enc.decode(y.item()) for y in ys]
+
+    @torch.no_grad()
+    def parse(self, sent: List[Word]) -> List[Head]:
+        """Predicted a dependency head for each word in a sentence."""
+        xs = encode_input(sent, self.char_enc)
+        embs = self.embed(xs)
+        scores = self.score_dep(embs)
+        ys = torch.argmax(scores, dim=1)
+        return [y.item() for y in ys]
+
+model = Joint(char_enc, pos_enc, emb_size=50, hid_size=200)
+
+def pos_accuracy(model, data: List[Tuple[EncInp, EncOut]]):
+    """Calculate the POS tagging accuracy of the model on the given dataset
+    of (encoded) input/output pairs."""
+    correct, total = 0, 0
+    for x, y in data:
+        pred_y = torch.argmax(model(x)[0], dim=1)
+        correct += (y[0] == pred_y).sum()
+        total += len(y[0])
+    return float(correct) / total
 
 def dep_accuracy(model, data: List[Tuple[EncInp, EncOut]]):
     """Calculate the head prediction accuracy of the model on the given dataset
     of (encoded) input/output pairs."""
     correct, total = 0, 0
     for x, y in data:
-        pred_y = torch.argmax(model(x), dim=1)
+        pred_y = torch.argmax(model(x)[1], dim=1)
         correct += (y[1] == pred_y).sum()
         total += len(y[1])
     return float(correct) / total
 
-def loss(pred: Tensor, gold: Tuple[Tensor, Tensor]) -> Tensor:
+def loss(pred: Tuple[Tensor, Tensor], gold: Tuple[Tensor, Tensor]) -> Tensor:
     criterion = nn.CrossEntropyLoss()
-    return criterion(pred, gold[1])
+    return criterion(pred[0], gold[0]) + criterion(pred[1], gold[1])
 
-train(model, enc_train, enc_dev, loss, dep_accuracy, epoch_num=10, learning_rate=0.001, report_rate=1)
+train(model, enc_train, enc_dev, loss, pos_accuracy, epoch_num=10, learning_rate=0.001, report_rate=1)
 train(model, enc_train, enc_dev, loss, dep_accuracy, epoch_num=10, learning_rate=0.0001, report_rate=1)
